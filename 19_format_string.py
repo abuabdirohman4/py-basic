5e27f2b4b617859ea2ee2e4439cc4c310adf53b7
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-05-06 07:27:54 +0700

    19. Format String & python version

---
+++ 19_format_string.py
@@ -0,0 +1,15 @@
+# format string
+
+
+# contoh generic
+nama = "marlene"
+str = "hello " + nama + ", apa kabar?"
+format_str = f"hello {nama}" 
+# print(str)
+# print(format_str)
+
+# angka
+angka = 2005.5
+# format_str = "angka = " + __builtins__.str(angka)
+format_str = f"angka = {angka}"
+print(format_str)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # py-basic
 my repo to learn python basic
+
+based playlist on [Kelas Terbuka](https://www.youtube.com/playlist?list=PLZS-MHyEIRo59lUBwU-XHH7Ymmb04ffOY)
